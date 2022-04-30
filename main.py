a3616f1225ea68d8a1b25c28bee846a385ef5562
Shashwat Sharma <68554481+shashwat73@users.noreply.github.com>
2022-04-29 20:08:42 -0400

    Code & lists

---
+++ main.py
@@ -0,0 +1,51 @@
+import random
+
+# How to play the game
+print("Wordle \nGuess the 5-letter Word, you have 6 guesses \n If the word you guessed has a letter in the right spot in the right word, it will be 'G' \n If the word you guessed has a letter of the right word, but in the wrong spot, it will be 'Y' \n If the letter isn't in the correct word, it will be '-' \n\n\n")
+
+# Defining the algorithm to check the user's guess
+def checkGuess(CorrectAnswer, userGuess):
+  position = 0
+  clue = ""
+
+  # Letting the user know if the position of the letter guessed is correct
+  for letter in userGuess:
+    if letter == CorrectAnswer[position]: # If in correct position, prints "G"
+      clue += "G"
+    elif letter in CorrectAnswer: # If in the answer, but not in the correct position, prints "Y"
+      clue += "Y"
+    else: # If letter isn't in the word, prints "-"
+      clue += "-"
+    position += 1
+  print(clue)
+  return clue == "GGGGG" # True if correct, False otherwise
+
+# Loading all of the words from "words.txt" and adding them to a list
+word_list = []
+word_file = open("words.txt")
+for word in word_file:
+  word_list.append(word.strip())
+
+# Picking a random word
+answer = random.choice(word_list)
+
+num_of_guesses = 0
+correctly_guessed = False
+
+while num_of_guesses < 6 and not correctly_guessed:
+  # Getting the guess from user
+  guess = input("Guess a 5-letter word: ")
+  print("You guessed", guess)
+  num_of_guesses += 1 # adding the number of total guesses
+  # Process the guess
+  correctly_guessed = checkGuess(answer, guess) # calling the function
+
+  # Letting the user know how many guesses are left
+  guesses_left = 6 - num_of_guesses
+  print("You have", guesses_left, "guesses left!\n\n")
+
+# End of game message
+if correctly_guessed:
+  print(colored("Congratulations! You guessed the correct word in"), num_of_guesses, "times!")
+else:
+  print("You are out of guesses, the correct word is", answer)
